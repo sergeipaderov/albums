ce63394f258b6be7ea057cd8bb07a196229d1f14
Serg Paderov <sergey.paderov@zup.com.br>
2018-11-22 15:26:44 -0200

    Created header

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import Header from './src/components/header'
+
+const App = () => (
+  <Header headerText={'Albums'}/>
+);
+
+export default App;
