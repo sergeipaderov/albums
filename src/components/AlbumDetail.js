bdb0f58620b0cc8c382e4eaa736e98a14948638b
Serg Paderov <sergey.paderov@zup.com.br>
2018-11-26 15:03:08 -0200

    created button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { headerContentStyle, thumbnailStyle } = styles;
+  const { title, artist, thumbnail_image, image, url } = album;
+  const { 
+    headerContentStyle, 
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={ thumbnailContainerStyle }>
           <Image
             style={ thumbnailStyle }
             source={{ uri: thumbnail_image }} 
           />
         </View>
         <View style={ headerContentStyle }>
-          <Text>{ title }</Text>
+          <Text style={ headerTextStyle }>{ title }</Text>
           <Text>{ artist }</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={ imageStyle } 
+          source= {{ uri: image }} 
+        />
+      </CardSection>
+      <CardSection>
+        <Button onPress={ () => Linking.openURL(url) }>
+          Buy now!
+        </Button>
       </CardSection>
     </Card>
   )
@@ -31,9 +49,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 
